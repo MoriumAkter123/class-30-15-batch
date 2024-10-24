5443d3739498f0613abaa193eb23378ae3cd5fe7
MoriumAkter123 <morium2312091068@diu.edu.bd>
2024-10-24 13:14:22 +0600

    my code

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+ let marks = [56, 61,90,30,58,98];
+
+// for(i=0; i<=5; i++){
+//     console.log(marks[i]);
+// }
+
+// for(i=0; i<=10; i++){
+
+// console.log("Hellow World",i);
+// }
+
+// var array = [344,59,0,67,99,90,55,88,55];
+
+//    array.forEach((element)=>{
+
+//        console.log( element);
+//    })
+
+// marks.forEach((element)=>{
+
+//     console.log(element);
+// })
+
+let i = 0;
+
+while(i<=5){
+    console.log(marks[i]);
+    i++;
+}
+
